c9102bed6e1648ce690e4dee7a07da11ad5085c3
James Tansey <57265750+JTansey93@users.noreply.github.com>
2025-05-12 16:37:11 +0100

    Removed some useless code lines

--- deepseek.py
+++ deepseek.py
@@ -5,39 +5,21 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-#Import the sys library to pass propmts through the command line
-#This mans we don' have to rewrite this file to update the prompt every time
-import sys
-
 load_dotenv()
 
 #The API key is in a file called .env
 #The file contains a line DEEPSEEK_API_KEY=<API key value>
 DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
 
-#sys.argv[0] is the actual script, sys.argv[1] is the first arg passed on the CLI
-#This means we can call the module in a terminal and pass it a string argument for the prompt
-if len(sys.argv) == 2:
-    PROMPT = sys.argv[1]
-
-    client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url='https://api.deepseek.com')
-
-    response = client.chat.completions.create(
-        model='deepseek-chat',
-        messages=[
-            {'role': 'system', 'content': 'You are a helpful AI assistant'},
-            {'role': 'user', 'content': PROMPT},
-            ],
-        stream=False
-)        
-
-    print(response.choices[0].message.content)
-
 if __name__ == '__main__':
     client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url='https://api.deepseek.com')
-    print('Enter your prompt:')
-    INPUT = input()
+
+    print('Enter your prompt and hit enter below, or use /bye and hit enter to quit.')
+    INPUT = input('>>>')
+
+    #TODO: Think of a better way to let the user edit the system prompt
     messages= [{'role': 'system', 'content': 'You are a helpful AI assistant'}]
+
     while INPUT != '/bye':
         messages.append({'role': 'user', 'content': INPUT})
         response = client.chat.completions.create(
@@ -46,5 +28,4 @@ if __name__ == '__main__':
                 )
         messages.append(response.choices[0].message)
         print(response.choices[0].message.content)
-        print('>>>', end='')
-        INPUT = input()
+        INPUT = input('>>>')
